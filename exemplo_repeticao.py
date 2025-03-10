14d7c62229cbf1b8fd1221088dc4a0be81b55b5f
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-03-10 10:56:43 -0300

    Add files via upload

---
+++ exemplo_repeticao.py
@@ -0,0 +1,9 @@
+
+for y in range(10): 
+    print(y)
+
+    # range (valor inicial, final, e quanto diminuiu)
+for y in range(10, -1, -1):
+    print(y)
+    if y == 0:
+        print("Explodiu Tudo!")
