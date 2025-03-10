285a598b4848e71be789ce4d1b6e809fabd540ce
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-03-10 11:53:12 -0300

    Delete exemplo_repeticao.py

--- exemplo_repeticao.py
+++
@@ -1,9 +0,0 @@
-
-for y in range(10): 
-    print(y)
-
-    # range (valor inicial, final, e quanto diminuiu)
-for y in range(10, -1, -1):
-    print(y)
-    if y == 0:
-        print("Explodiu Tudo!")
